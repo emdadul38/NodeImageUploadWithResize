8b19380cf49f26bd8cdb00eeaa3037ed84ef357e
emdadul38 <emdadul225@gmail.com>
2021-05-26 20:50:11 +0600

    For multiple image upload

--- router.js
+++ router.js
@@ -10,14 +10,17 @@ router.get('/', async function (req, res) {
   await res.render('index');
 });
 
-router.post('/post', upload.single('image'), async function (req, res) {
+router.post('/post', upload.any(), async function (req, res) {
+  console.log(req.files)
   const imagePath = path.join(__dirname, '/public/images');
   const fileUpload = new Resize(imagePath);
-  if (!req.file) {
+  if (!req.files) {
     res.status(401).json({error: 'Please provide an image'});
   }
-  const filename = await fileUpload.save(req.file.buffer);
-  return res.status(200).json({ name: filename });
+  for(let file of req.files) {
+    await fileUpload.save(file.buffer);
+  }
+  return res.status(200).json({message:  `File upload `});
 });
 
 module.exports = router;
