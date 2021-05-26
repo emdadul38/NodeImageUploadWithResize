4496290f783ea55851cdef056944d6c89ae073c0
emdadul38 <emdadul225@gmail.com>
2021-05-26 15:50:00 +0600

    Image upload with resize using Sharp package

---
+++ Resize.js
@@ -0,0 +1,29 @@
+const sharp = require('sharp');
+const uuidv4 = require('uuid/v4');
+const path = require('path');
+
+class Resize {
+  constructor(folder) {
+    this.folder = folder;
+  }
+  async save(buffer) {
+    const filename = Resize.filename();
+    const filepath = this.filepath(filename);
+
+    await sharp(buffer)
+      .resize(608, 1080, {
+        fit: sharp.fit.cover,
+        position: sharp.strategy.cover
+      })
+      .toFile(filepath);
+    
+    return filename;
+  }
+  static filename() {
+    return `${uuidv4()}.png`;
+  }
+  filepath(filename) {
+    return path.resolve(`${this.folder}/${filename}`)
+  }
+}
+module.exports = Resize;
